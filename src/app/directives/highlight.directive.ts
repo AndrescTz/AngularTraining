cb29bd42a3287850ea14de01272ae27510ec3e9e
Andres Caycho <andres.cytz@gmail.com>
2017-10-24 14:17:44 -0500

    Custom directive added

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,8 +1,16 @@
-import { Directive, OnInit, ElementRef } from '@angular/core';
+import {Directive, ElementRef, OnInit, Renderer2, Input} from "@angular/core";
 
-@Directive({ selector: '[highlight]' })
+@Directive({
+  selector: '[highlight]'
+})
 
-export class HighlightDirective implements OnInit {
-    constructor(private elRef: ElementRef) {}
-    ngOnInit(){}
+export class HighlightDirective implements OnInit{
+  constructor(private elRef: ElementRef, private renderer: Renderer2){}
+  @Input('highlight') plan: string = '';
+  ngOnInit(){
+    if(this.plan === 'pay'){
+      this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'yellow');
+      this.renderer.setStyle(this.elRef.nativeElement, 'font-weight', 'bold');
+    }
+  }
 }
